65d4837726859c5695cc4b7183cc4a8b16bb7042
MarySnopok <marysnopok8@gmail.com>
2022-01-11 17:27:08 +0100

    eslint fix

--- frontend/src/PrivateScreen.js
+++ frontend/src/PrivateScreen.js
@@ -3,9 +3,7 @@ import React from "react";
 // MUI components import
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 
 export const Private = () => {
   return (
